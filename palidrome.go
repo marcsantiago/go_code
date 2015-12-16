f8b212f55a34b55a330aeb77d351111b096e861f
marc-pubgears <marc@pubgears.com>
2015-12-15 22:30:15 -0500

    palidrome code

--- palidrome.go
+++ palidrome.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	//"strings"
 )
 
 func main() {
 	var str string
 	fmt.Scan(&str)
+	fmt.Println(isPalidrome(str))
+}
 
+func isPalidrome(str string) bool {
+	var rev_str string
+	for i := len(str) - 1; i >= 0; i-- {
+		rev_str += string(str[i])
+	}
+	if str == rev_str {
+		return true
+	}
+	return false
 }
