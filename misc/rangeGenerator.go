1849fa996ac5774557487842c5b8f48c0f32525b
marc-pubgears <marc@pubgears.com>
2015-12-17 19:04:00 -0500

    a

---
+++ misc/rangeGenerator.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+type RangeLister struct {
+	list []int
+}
+
+func (r RangeLister) add() int[] {
+
+}
+
+func main() {
+	print := fmt.Println
+	rl := new(RangeLister)
+	print(rl.add())
+
+}
