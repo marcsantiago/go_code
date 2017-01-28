7875ca11b9f914fdd2fc28d7a5315790e242fb19
marc santiago <marcanthonysanti@gmail.com>
2017-01-28 12:56:41 -0500

    base poker programmer

--- poker.go
+++ poker.go
@@ -51,19 +51,33 @@ var (
 		"3 of a kind 2":     112,
 		"3 of a kind 1":     111,
 
-		"pair of a kind ace":   110,
-		"pair of a kind king":  105,
-		"pair of a kind queen": 100,
-		"pair of a kind jack":  95,
-		"pair of a kind 9":     89,
-		"pair of a kind 8":     88,
-		"pair of a kind 7":     87,
-		"pair of a kind 6":     86,
-		"pair of a kind 5":     85,
-		"pair of a kind 4":     84,
-		"pair of a kind 3":     83,
-		"pair of a kind 2":     82,
-		"pair of a kind 1":     81,
+		"high pair of a kind ace":   110,
+		"high pair of a kind king":  105,
+		"high pair of a kind queen": 100,
+		"high pair of a kind jack":  95,
+		"high pair of a kind 9":     89,
+		"high pair of a kind 8":     88,
+		"high pair of a kind 7":     87,
+		"high pair of a kind 6":     86,
+		"high pair of a kind 5":     85,
+		"high pair of a kind 4":     84,
+		"high pair of a kind 3":     83,
+		"high pair of a kind 2":     82,
+		"high pair of a kind 1":     81,
+
+		"pair of a kind ace":   80,
+		"pair of a kind king":  79,
+		"pair of a kind queen": 78,
+		"pair of a kind jack":  77,
+		"pair of a kind 9":     76,
+		"pair of a kind 8":     75,
+		"pair of a kind 7":     74,
+		"pair of a kind 6":     73,
+		"pair of a kind 5":     72,
+		"pair of a kind 4":     71,
+		"pair of a kind 3":     70,
+		"pair of a kind 2":     69,
+		"pair of a kind 1":     68,
 	}
 )
 
@@ -98,10 +112,10 @@ type Card struct {
 
 // Player ...
 type Player struct {
-	Name  string
-	Total int
-	Wins  int
-	Cards Hand
+	Name       string
+	TotalScore int
+	HandsWon   int
+	Cards      Hand
 }
 
 func (d *Deck) createDeck() {
@@ -143,6 +157,15 @@ func (d *Deck) createDeck() {
 			IsAce:     true,
 		})
 	}
+}
+
+func inSlice(slice []string, tar string) bool {
+	for _, s := range slice {
+		if s == tar {
+			return true
+		}
+	}
+	return false
 }
 
 func (d *Deck) shuffle() {
@@ -283,9 +306,10 @@ func checkHand(hand Hand) (string, int) {
 	}
 
 	// determine if 4 of a kind, 3 or a kind, or 2 of a kind
+	twoPairs := []string{}
 	for _, card := range hand {
 		cards := findCardsByValue(hand, card.Value, card.IsRoyal, card.RoyalType)
-		// fmt.Printf("card: %v  cards: %v  len: %d\n", c, cards, len(cards))
+
 		if len(cards) == 4 {
 			if cards[0].IsAce {
 				return "4 of a kind ace", 0
@@ -306,13 +330,28 @@ func checkHand(hand Hand) (string, int) {
 		}
 		if len(cards) == 2 {
 			if cards[0].IsAce {
-				return "pair of a kind ace", 0
+				if !inSlice(twoPairs, "pair of a kind ace") {
+					twoPairs = append(twoPairs, "pair of a kind ace")
+				}
+
 			} else if cards[0].IsRoyal {
-				return fmt.Sprintf("pair of a kind %s", cards[0].RoyalType), 0
+				if !inSlice(twoPairs, fmt.Sprintf("pair of a kind %s", cards[0].RoyalType)) {
+					twoPairs = append(twoPairs, fmt.Sprintf("pair of a kind %s", cards[0].RoyalType))
+				}
 			} else {
-				return fmt.Sprintf("pair of a kind %d", cards[0].Value), 0
+				if !inSlice(twoPairs, fmt.Sprintf("pair of a kind %d", cards[0].Value)) {
+					twoPairs = append(twoPairs, fmt.Sprintf("pair of a kind %d", cards[0].Value))
+				}
 			}
 		}
+	}
+	if len(twoPairs) == 2 {
+		if score[twoPairs[0]] > score[twoPairs[1]] {
+			return "high " + twoPairs[0], 0
+		}
+		return "high " + twoPairs[1], 0
+	} else if len(twoPairs) == 1 {
+		return twoPairs[0], 0
 	}
 
 	// logic here is broken
@@ -358,11 +397,54 @@ func (p *Player) removeCard(selected []int) {
 			j := i - deleted
 			if p.Cards[j] == card {
 				p.Cards = p.Cards[:j+copy(p.Cards[j:], p.Cards[j+1:])]
-				fmt.Printf("here: %v   %d\n\n", p.Cards, len(p.Cards))
 				deleted++
 			}
 		}
 	}
+}
+
+func computerLogic(comp *Player, deck *Deck) (string, int) {
+	// first check the hand to see of the score is high enough to do nothing
+	// computer will stay with a hand of 3 of a kind or greater
+	handName, baseScore := checkHand(comp.Cards)
+	compScore := baseScore
+	if baseScore > 0 {
+		compScore = score[handName]
+	}
+	if compScore >= 111 {
+		return handName, 0
+	}
+	// determine how many and which cards to removeCard
+	// later I will go back and add probalities of how many cards to discard to maximize the handName
+	// for now just to get the game working I'm going to randomize 0-5
+	t := time.Now()
+	source := rand.NewSource(t.Unix())
+	r := rand.New(source)
+	n := r.Perm(6)[0] // number of cards to drop
+
+	selected := []int{}
+	for i := 0; i < n; i++ {
+		t := time.Now()
+		source := rand.NewSource(t.Unix())
+		r := rand.New(source)
+		n := r.Perm(6)[0] + 1
+		contains := func(n int, sel []int) bool {
+			for _, s := range selected {
+				if n == s {
+					return true
+				}
+			}
+			return false
+		}(n, selected)
+		if !contains {
+			selected = append(selected, n)
+		}
+	}
+	comp.removeCard(selected)
+	for i := 0; i < len(selected); i++ {
+		comp.Cards = append(comp.Cards, deck.draw())
+	}
+	return checkHand(comp.Cards)
 }
 
 func main() {
@@ -413,32 +495,44 @@ func main() {
 			}
 			fmt.Printf("New hand\n")
 			showHand(&player)
-			break
-		}
+			fmt.Printf("\n\n")
 
-		// another game?
-		for {
-			fmt.Println("play again? [y, n]")
-			var action string
-			fmt.Scan(&action)
-			if strings.Contains(strings.ToLower(action), "y") {
-				fmt.Printf("\n\n")
-				break
+			player1HandName, baseScore := checkHand(player.Cards)
+			player1Score := baseScore
+			if player1Score == 0 {
+				player1Score = score[player1HandName]
 			}
-			return
+			player.TotalScore = player.TotalScore + player1Score
+
+			computerHandName, baseScore := computerLogic(&computer, &deck)
+			computerScore := baseScore
+			if computerScore == 0 {
+				computerScore = score[computerHandName]
+			}
+			computer.TotalScore = computer.TotalScore + computerScore
+
+			if computerScore > player1Score {
+				fmt.Printf("Computer won with the hand: %s score: %d\n", computerHandName, computerScore)
+				computer.HandsWon++
+			} else {
+				fmt.Printf("Player1 won with the hand: %s score: %d\n", player1HandName, player1Score)
+				player.HandsWon++
+			}
+
+			// another game?
+			for {
+				fmt.Printf("play again? [y, n]\n")
+				var action string
+				fmt.Scan(&action)
+				if strings.Contains(strings.ToLower(action), "y") {
+					fmt.Printf("\n\n")
+					break
+				}
+				fmt.Printf("Player1 final score: %d\nPlayer1 total hands won %d\n", player.TotalScore, player.HandsWon)
+				fmt.Printf("Computer final score: %d\nComputer total hands won %d\n", computer.TotalScore, computer.HandsWon)
+				return
+			}
 		}
+
 	}
-
-	// ph, _ := checkHand(player.Cards)
-	// ch, _ := checkHand(computer.Cards)
-	//
-	// for _, c := range player.Cards {
-	// 	fmt.Println(c)
-	// }
-	// fmt.Printf("hand: %s score: %d\n\n", ph, score[ph])
-	// for _, c := range computer.Cards {
-	// 	fmt.Println(c)
-	// }
-	// fmt.Printf("hand: %s score: %d\n\n", ch, score[ch])
-
 }
