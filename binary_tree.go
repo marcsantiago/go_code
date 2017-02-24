a17a2f5bffacd9cc6942ab6a2edcdc6406f15bdc
Marc Santiago <marcanthonysanti@gmail.com>
2017-02-23 22:45:48 -0500

    using private public conventions

--- binary_tree.go
+++ binary_tree.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"strconv"
-	"strings"
 )
 
 type node struct {
@@ -14,23 +10,25 @@ type node struct {
 	Data  int
 }
 
-type tree struct {
+// Tree ...
+type Tree struct {
 	Root *node
 }
 
-func (t *tree) findRoot(data int) bool {
+// FindRoot ...
+func (t *Tree) FindRoot(data int) bool {
 	newNode := node{
 		Data: data,
 	}
 	if t.Root != nil {
-		if t.findRootLogic(t.Root, newNode) != nil {
+		if t.findRoot(t.Root, newNode) != nil {
 			return true
 		}
 	}
 	return false
 }
 
-func (t *tree) findRootLogic(search *node, target node) *node {
+func (t *Tree) findRoot(search *node, target node) *node {
 	var returnNode *node
 	if search == nil {
 		return returnNode
@@ -39,15 +37,16 @@ func (t *tree) findRootLogic(search *node, target node) *node {
 		return search
 	}
 
-	returnNode = t.findRootLogic(search.Left, target)
+	returnNode = t.findRoot(search.Left, target)
 	if returnNode == nil {
-		returnNode = t.findRootLogic(search.Right, target)
+		returnNode = t.findRoot(search.Right, target)
 	}
 	return returnNode
 
 }
 
-func (t *tree) add(data int) {
+// Add ...
+func (t *Tree) Add(data int) {
 	nodeToAdd := node{
 		Data: data,
 	}
@@ -58,59 +57,61 @@ func (t *tree) add(data int) {
 		t.Root = &nodeToAdd
 		return
 	}
-	t.tranverseAdd(t.Root, nodeToAdd)
+	t.add(t.Root, nodeToAdd)
 	return
 }
 
-func (t *tree) tranverseAdd(oldnode *node, newNode node) {
+func (t *Tree) add(oldnode *node, newNode node) {
 	if newNode.Data < oldnode.Data {
 		if oldnode.Left == nil {
 			oldnode.Left = &newNode
 		} else {
-			t.tranverseAdd(oldnode.Left, newNode)
+			t.add(oldnode.Left, newNode)
 		}
 	} else if newNode.Data > oldnode.Data {
 		if oldnode.Right == nil {
 			oldnode.Right = &newNode
 		} else {
 
-			t.tranverseAdd(oldnode.Right, newNode)
+			t.add(oldnode.Right, newNode)
 		}
 	}
 	return
 }
 
-func (t *tree) inOrderTraversal() {
+// InOrderTraversal ...
+func (t *Tree) InOrderTraversal() {
 	if t.Root != nil {
 		currentNode := t.Root
 		if currentNode.Left == nil && currentNode.Right == nil {
 			fmt.Println(currentNode.Data)
 		} else {
-			t.inOrderTraversalLogic(currentNode)
+			t.inOrderTraversal(currentNode)
 		}
 	}
 	return
 }
 
-func (t *tree) inOrderTraversalLogic(n *node) {
+func (t *Tree) inOrderTraversal(n *node) {
 	if n.Left != nil {
-		t.inOrderTraversalLogic(n.Left)
+		t.inOrderTraversal(n.Left)
 	}
 	fmt.Println(n.Data)
 	if n.Right != nil {
-		t.inOrderTraversalLogic(n.Right)
+		t.inOrderTraversal(n.Right)
 	}
 	return
 }
 
-func (t *tree) countEdges() (edges int) {
+// CountEdges ...
+func (t *Tree) CountEdges() (edges int) {
 	c := make(chan int, 10)
 	if t.Root != nil {
 		currentNode := t.Root
 		if currentNode.Left == nil && currentNode.Right == nil {
 			return 1
 		}
-		t.countEdgesLogic(currentNode, c)
+		t.countEdges(currentNode, c)
 	}
 
 	for {
@@ -122,12 +123,12 @@ func (t *tree) countEdges() (edges int) {
 		edges++
 
 	}
-	return edges + 1 // connecting the root as 1 edge
+	return edges
 }
 
-func (t *tree) countEdgesLogic(n *node, counter chan int) {
+func (t *Tree) countEdges(n *node, counter chan int) {
 	if n.Left != nil {
-		go t.countEdgesLogic(n.Left, counter)
+		go t.countEdges(n.Left, counter)
 	}
 
 	if n.Left == nil && n.Right == nil {
@@ -137,55 +138,14 @@ func (t *tree) countEdgesLogic(n *node, counter chan int) {
 	}
 
 	if n.Right != nil {
-		go t.countEdgesLogic(n.Right, counter)
+		go t.countEdges(n.Right, counter)
 	}
 	return
 }
 
 func main() {
-	// BASED ON http://rosalind.info/problems/tree/
-	// NOT CORRECT YET
-	t := new(tree)
-	b, err := ioutil.ReadFile("data.txt")
-	if err != nil {
-		panic(err)
-	}
-	s := string(b)
-	parts := strings.Split(s, "\n")
-	n := parts[0]
-
-	startNode, _ := strconv.Atoi(n)
-	t.add(startNode)
-
-	for {
-		counter := 1
-		oldSize := len(parts) - 1
-		newSize := oldSize
-		for _, part := range parts[1:] {
-			nums := strings.Split(part, " ")
-			if len(nums) == 2 {
-				toAdd, _ := strconv.Atoi(nums[0])
-				root, _ := strconv.Atoi(nums[1])
-				if t.findRoot(root) {
-					t.add(toAdd)
-					parts = parts[:counter+copy(parts[counter:], parts[counter+1:])]
-				} else if t.findRoot(toAdd) {
-					t.add(root)
-					parts = parts[:counter+copy(parts[counter:], parts[counter+1:])]
-				}
-
-			}
-			counter++
-		}
-		oldSize = len(parts) - 1
-		if newSize == oldSize {
-			break
-		}
-
-	}
-
-	b, _ = json.MarshalIndent(t, "", " ")
-	fmt.Println(string(b))
-	fmt.Println(t.countEdges())
-
+	t := Tree{}
+	t.Add(50)
+	t.Add(10)
+	t.Add(60)
 }
